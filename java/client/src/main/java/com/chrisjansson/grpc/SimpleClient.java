f72b1bf024446232c92c61936e734eacbc749c3b
Chris Jansson <chrisjansson95@gmail.com>
2026-02-13 14:04:39 -0600

    Reproduce shutdown issue

--- java/client/src/main/java/com/chrisjansson/grpc/SimpleClient.java
+++ java/client/src/main/java/com/chrisjansson/grpc/SimpleClient.java
@@ -3,6 +3,9 @@ package com.chrisjansson.grpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
+import io.grpc.health.v1.HealthCheckRequest;
+import io.grpc.health.v1.HealthCheckResponse;
+import io.grpc.health.v1.HealthGrpc;
 import lombok.extern.java.Log;
 import samplepb.SampleServiceGrpc;
 import samplepb.SampleProto.Request;
@@ -17,9 +20,34 @@ public class SimpleClient {
                 .usePlaintext()
                 .build();
 
+        HealthGrpc.HealthStub healthStub = HealthGrpc.newStub(channel);
+
+        log.info("Calling Watch RPC on health service");
+
+        HealthCheckRequest healthRequest = HealthCheckRequest.newBuilder()
+                .setService("")  // Empty string checks overall server health
+                .build();
+
+        healthStub.watch(healthRequest, new StreamObserver<HealthCheckResponse>() {
+            @Override
+            public void onNext(HealthCheckResponse response) {
+                log.info("Health status update: " + response.getStatus());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.severe("Error in health watch: " + t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                log.info("Health watch stream completed");
+            }
+        });
+
         SampleServiceGrpc.SampleServiceStub stub = SampleServiceGrpc.newStub(channel);
 
-        log.info("Calling RPC");
+        log.info("Calling ProcessMessage RPC");
 
         StreamObserver<Request> requestObserver = stub.processMessage(new StreamObserver<Response>() {
             @Override
